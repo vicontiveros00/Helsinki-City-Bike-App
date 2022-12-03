4a87bb73c76f6f860c2c554b6cc56ac4dc21e7c9
Vic <vontiverosewhs@gmail.com>
2022-12-03 14:50:05 +0200

    Small improvements to base app

--- bike-app/src/App.jsx
+++ bike-app/src/App.jsx
@@ -5,16 +5,21 @@ function App() {
   const [start, setStart] = useState('');
   const [destination, setDestination] = useState('');
   const [distance, setDistance] = useState(null);
+  const url = 'http://127.0.0.1:8090/api/collections/';
 
   const findJourneyData = async(start, end) => {
-    const res = await fetch(`http://127.0.0.1:8090/api/collections/journeys/records?filter=departure_station_name='${start}'&&return_station_name='${end}'`, {
-      cache: 'no-store',
-    });
-    const data = await res.json();
-    console.log(data.items[0].distance_m);
-    setDistance(data.items[0].distance_m);
-    return data.items[0];
-  }
+    try {
+      const res = await fetch(`${url}journeys/records?filter=departure_station_name='${start}'&&return_station_name='${end}'`, {
+        cache: 'no-store',
+      });
+      const data = await res.json();
+      console.log(data.items[0].distance_m);
+      setDistance(data.items[0].distance_m);
+      return data.items[0];
+    } catch(err) {
+      setDistance('noroute');
+    }
+  } 
 
   return (
     <>
@@ -31,7 +36,7 @@ function App() {
         onChange={(e) => setDestination(e.target.value)}
       />
       <button type="submit" onClick={() => findJourneyData(start, destination)}>find</button>
-      <p>{distance && `Average distance from ${start} station and ${destination} station is ${distance} meters`}</p>
+      <p>{distance && distance !== 'noroute' ? `Average distance between stations is ${distance} meters.` : 'No route found'}</p>
     </>
   )
 }
