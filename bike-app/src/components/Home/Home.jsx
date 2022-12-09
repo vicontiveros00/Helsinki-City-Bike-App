5fdbb2ca6bad8584430a70e54f8e25e7c3deac86
Vic <vontiverosewhs@gmail.com>
2022-12-09 21:32:24 +0200

    Fixed figcaption line height

--- bike-app/src/components/Home/Home.jsx
+++ bike-app/src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home(props) {
         <div className="home">
             <h2>HEL City Bikes <span className="bike">🚲</span></h2>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Helsinki_city_bikes_station_2016_kauppatori.jpg/800px-Helsinki_city_bikes_station_2016_kauppatori.jpg?20160930172854" />
-                <figcaption>Image used under Creative Commons license. Data is owned by City Bike Finland and HSL City Bike Stations.</figcaption>
+                <figcaption>Image used under Creative Commons license. Data is owned by City Bike Finland and HSL.</figcaption>
                 <p>Moi! Welcome to HEL City Bikes app. Here you can search Helsinki and Espoo's rental bike stations and view all journeys taken in the summer of 2021.</p>
                 {longestJourney ? <p>The longest journey was {(longestJourney / 1000).toFixed(2)} km!</p> : 
                     <PulseLoader color="#646cff" cssOverride={{
